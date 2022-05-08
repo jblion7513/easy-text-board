1c5fa29c260722f03c3bdc7fde5f5b7a5c36a163
jblion7513 <jblion7513@gmail.com>
2022-05-08 08:57:55 -0500

    조건문과 반복문 사용해서 여러가지 기능을 만듬

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -5,15 +5,50 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		String command;
+		int inputNo=0;
+		String title1="";
+		String body1="";
+		String title2="";
+		String body2="";
 		Scanner scanner=new Scanner(System.in);
 		while(true) {
-			System.out.printf("명령어 )");
+			System.out.printf("명령어) ");
 			command=scanner.nextLine();
 			if(command.equals("article add")) {
-				System.out.println("== 게시물 등록 ==");
+				if(inputNo==0) {
+					inputNo++;
+					System.out.println("== 게시물 등록 ==");
+					System.out.printf("제목) ");
+					title1=scanner.nextLine();
+					System.out.printf("내용) ");
+					body1=scanner.nextLine();
+					System.out.printf("%d번 게시물이 등록되었습니다.\n",inputNo);
+				}
+				if(inputNo==1) {
+					inputNo++;
+					System.out.println("== 게시물 등록 ==");
+					System.out.printf("제목) ");
+					title2=scanner.nextLine();
+					System.out.printf("내용) ");
+					body2=scanner.nextLine();
+					System.out.printf("%d번 게시물이 등록되었습니다.\n",inputNo);
+				}
+				
 			}
 			else if(command.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
+				if(inputNo==0) {
+					System.out.println("등록된 게시물이 없습니다.");
+				}
+				else {
+					if(inputNo==1) {
+						System.out.printf("%d번, 제목: %s / 내용: %s\n",1,title1,body1);
+					}
+					else if(inputNo==2) {
+						System.out.printf("%d번, 제목: %s / 내용: %s\n",1,title1,body1);
+						System.out.printf("%d번, 제목: %s / 내용: %s\n",2,title2,body2);
+					}
+				}
 			}
 			else if(command.equals("exit")) {
 				System.out.println("프로그램을 종료합니다.");
