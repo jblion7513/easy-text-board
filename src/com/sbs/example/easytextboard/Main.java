ae10417c29574bbc79d673c2702e6c7f02f30b72
jblion7513 <jblion7513@gmail.com>
2022-05-08 01:05:47 -0500

    while, 종료하기 전까지 무한히 실행

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -6,16 +6,22 @@ public class Main {
 	public static void main(String[] args) {
 		String command;
 		Scanner scanner=new Scanner(System.in);
-		System.out.printf("명령어 )");
-		command=scanner.nextLine();
-		if(command.equals("article add")) {
-			System.out.println("== 게시물 등록 ==");
-		}
-		else if(command.equals("article list")) {
-			System.out.println("== 게시물 리스트 ==");
-		}
-		else {
-			System.out.println("잘못된 명령어입니다.");
+		while(true) {
+			System.out.printf("명령어 )");
+			command=scanner.nextLine();
+			if(command.equals("article add")) {
+				System.out.println("== 게시물 등록 ==");
+			}
+			else if(command.equals("article list")) {
+				System.out.println("== 게시물 리스트 ==");
+			}
+			else if(command.equals("exit")) {
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			}
+			else {
+				System.out.println("잘못된 명령어입니다.");
+			}
 		}
 		scanner.close();
 	}
