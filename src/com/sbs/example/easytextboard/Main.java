737c07fab92a9d624e21e7b6cd9b014b5fd6b817
jblion7513 <jblion7513@gmail.com>
2022-05-08 01:00:44 -0500

    스캐너로 고객에게 명령어 입력받기

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -1,9 +1,23 @@
 package com.sbs.example.easytextboard;
 
-public class Main {
+import java.util.Scanner;
 
-public static void main(String[] args) {
-	System.out.println("안녕");
-}
+public class Main {
+	public static void main(String[] args) {
+		String command;
+		Scanner scanner=new Scanner(System.in);
+		System.out.printf("명령어 )");
+		command=scanner.nextLine();
+		if(command.equals("article add")) {
+			System.out.println("== 게시물 등록 ==");
+		}
+		else if(command.equals("article list")) {
+			System.out.println("== 게시물 리스트 ==");
+		}
+		else {
+			System.out.println("잘못된 명령어입니다.");
+		}
+		scanner.close();
+	}
 
 }
